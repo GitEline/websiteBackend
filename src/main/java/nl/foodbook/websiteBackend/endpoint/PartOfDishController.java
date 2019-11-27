eef0e01621e84eb4c0a883b188d244985edecbe0
ekompanje <ekompanje@gmail.com>
2019-11-27 13:52:37 +0100

    Eight commit, met crosslink

--- src/main/java/nl/foodbook/websiteBackend/endpoint/PartOfDishController.java
+++ src/main/java/nl/foodbook/websiteBackend/endpoint/PartOfDishController.java
@@ -3,13 +3,11 @@ package nl.foodbook.websiteBackend.endpoint;
 import nl.foodbook.websiteBackend.domain.PartOfDish;
 import nl.foodbook.websiteBackend.service.PartOfDishService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class PartOfDishController {
 
--- src/main/java/nl/foodbook/websiteBackend/endpoint/RecipeController.java
+++ src/main/java/nl/foodbook/websiteBackend/endpoint/RecipeController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class RecipeController {
 
